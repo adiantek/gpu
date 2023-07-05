b94b206eca697dc273292c5f15019f1c45a28faa
Adrian Antkowiak <adiantek@gmail.com>
2023-07-05 15:44:29 +0200

    advect velocity & dye

--- include/fluids.cuh
+++ include/fluids.cuh
@@ -4,9 +4,10 @@
 #include <device_launch_parameters.h>
 #include <driver_types.h>
 #include <helper_cuda.h>
+
 #include <Controller.hpp>
 
-extern float3 *h_velocity[2];
+extern float2 *h_velocity[2];
 extern size_t h_velocity_pitch[2];
 extern float *h_pressure[2];
 extern size_t h_pressure_pitch[2];
@@ -15,60 +16,63 @@ extern size_t h_divergence_pitch;
 extern float3 *h_dye[2];
 extern size_t h_dye_pitch[2];
 
-__global__ void boundary_advect(float **result, float **field, float scale, int width, int height);
-
+template <typename T>
+__global__ void boundary_advect_kernel(T *result, size_t result_pitch,
+                                       T *field, size_t field_pitch,
+                                       float scale, int width, int height);
 
 // interior_advect.fs
 
-
 /**
  * @brief Advection Fragment Program
- * 
+ *
  * @param result result
- * @param timestep 
+ * @param timestep
  * @param rdx reciprocal of the grid scale x
  * @param x Qty to be advected
  * @param u Velocity profile
  * @param width array width
  * @param height array height
- * @return __global__ 
+ * @return __global__
  */
-__global__ void advect(float **result, float timestep, float rdx, float **x, float **u, int width, int height);
+template <typename T>
+__global__ void advect_kernel(T *result, size_t result_pitch,
+                       float timestep, float rdx,
+                       T *x, size_t x_pitch,
+                       float2 *u, size_t u_pitch,
+                       int width, int height);
 
 // solve_jacobi.fs
 
 /**
  * @brief The Jacobi Iteration Fragment Program Used to Solve Poisson Equations
- * 
+ *
  * @param result result
  * @param x x vector (Ax = b)
  * @param b b vector (Ax = b)
- * @param alpha 
+ * @param alpha
  * @param rBeta reciprocal beta
  * @param width array width
  * @param height array height
  */
-__global__ void jacobi_float3_kernel(float3 *result, size_t result_pitch,
-    float3 *x, size_t x_pitch,
-    float3 *b, size_t b_pitch,
-    float alpha, float rBeta, int width, int height);
-__global__ void jacobi_float_kernel(float *result, size_t result_pitch,
-    float *x, size_t x_pitch,
-    float *b, size_t b_pitch,
-    float alpha, float rBeta, int width, int height);
+template <typename T>
+void jacobi_float2_kernel(T *result, size_t result_pitch,
+                          T *x, size_t x_pitch,
+                          T *b, size_t b_pitch,
+                          float alpha, float rBeta, int width, int height);
 
 // divergence.fs
 
 /**
  * @brief The Divergence Fragment Program
- * 
+ *
  * @param result divergence
  * @param halfrdx 0.5 / gridscale
  * @param wX vector field (x component)
  * @param wY vector field (y component)
  * @param width array width
  * @param height array height
- * @return __global__ 
+ * @return __global__
  */
 __global__ void divergence(float **result, float halfrdx, float **wX, float **wY, int width, int height);
 
--- src/fluids.cu
+++ src/fluids.cu
@@ -1,7 +1,8 @@
-#include <fluids.cuh>
 #include <helper_math.h>
 
-__device__ float3 *m_velocity[2];
+#include <fluids.cuh>
+
+__device__ float2 *m_velocity[2];
 __device__ size_t m_velocity_pitch[2];
 __device__ float *m_pressure[2];
 __device__ size_t m_pressure_pitch[2];
@@ -10,7 +11,7 @@ __device__ size_t m_divergence_pitch;
 __device__ float3 *m_dye[2];
 __device__ size_t m_dye_pitch[2];
 
-float3 *h_velocity[2];
+float2 *h_velocity[2];
 size_t h_velocity_pitch[2];
 float *h_pressure[2];
 size_t h_pressure_pitch[2];
@@ -19,160 +20,137 @@ size_t h_divergence_pitch;
 float3 *h_dye[2];
 size_t h_dye_pitch[2];
 
-__global__ void boundary_advect(float **result, float **field, float scale, int width, int height) {
+template <typename T>
+__global__ void boundary_advect_kernel(T *result, size_t result_pitch,
+                                       T *field, size_t field_pitch,
+                                       float scale, int width, int height) {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     int j = blockIdx.y * blockDim.y + threadIdx.y;
 
-    if (i == 0) { // left
-        i = 1;
+    if (i >= width || j >= height) return;
+
+    int k = i;
+    int l = j;
+
+    if (k == 0) {  // left
+        k = 1;
     }
-    if (i >= width - 1) { // right
-        i = width - 2;
+    if (k >= width - 1) {  // right
+        k = width - 2;
     }
-    if (j == 0) { // bottom
-        j = 1;
+    if (l == 0) {  // bottom
+        l = 1;
     }
-    if (j >= height - 1) { // top
-        j = height - 2;
+    if (l >= height - 1) {  // top
+        l = height - 2;
     }
-    result[j][i] = field[j][i] * scale;
+    if (k == i && l == j) {
+        scale = 1.0f;
+    }
+    result[j * result_pitch / sizeof(T) + i] = field[l * field_pitch / sizeof(T) + k] * scale;
 }
 
 /**
  * @brief Advection Fragment Program
- * 
+ *
  * @param result result
- * @param timestep 
+ * @param timestep
  * @param rdx reciprocal of the grid scale x
  * @param x Qty to be advected
  * @param u Velocity profile
  * @param width array width
  * @param height array height
- * @return __global__ 
+ * @return __global__
  */
-__global__ void advect(float **result, float timestep, float rdx, float **x, float **u, int width, int height) {
+template <typename T>
+__global__ void advect_kernel(T *result, size_t result_pitch,
+                       float timestep, float rdx,
+                       T *x, size_t x_pitch,
+                       float2 *u, size_t u_pitch,
+                       int width, int height) {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     int j = blockIdx.y * blockDim.y + threadIdx.y;
 
     if (i >= width || j >= height) return;
-    if (i == 0 || j == 0) return;
+    if (i == 0 || j == 0 || i == width - 1 || j == height -1) {
+        result[j * result_pitch / sizeof(T) + i] = x[j * x_pitch / sizeof(T) + i];
+        return;
+    }
+    float2 pos = make_float2(i, j) - timestep * rdx * u[j * u_pitch / sizeof(float2) + i];
 
-    float posX = i - timestep * rdx * u[j][i];
-    float posY = j - timestep * rdx * u[j][i];
+    float2 floorPos = floorf(pos);
+    float2 fracPos = pos - floorPos;
 
-    int i0 = floor(posX);
-    int i1 = ceil(posX);
-    int j0 = floor(posY);
-    int j1 = ceil(posY);
+    int2 iFloorPos = make_int2(floorPos);
 
-    if (i0 < 0) i0 = 0;
-    if (i0 >= width) i0 = width - 1;
+    if (iFloorPos.x < 0) iFloorPos.x = 0;
+    if (iFloorPos.x >= width) iFloorPos.x = width - 1;
+    if (iFloorPos.y < 0) iFloorPos.y = 0;
+    if (iFloorPos.y >= height) iFloorPos.y = height - 1;
 
-    if (i1 < 0) i1 = 0;
-    if (i1 >= width) i1 = width - 1;
+    T x00 = x[iFloorPos.y * x_pitch / sizeof(T) + iFloorPos.x];
+    T x01 = x[iFloorPos.y * x_pitch / sizeof(T) + iFloorPos.x + 1];
+    T x10 = x[(iFloorPos.y + 1) * x_pitch / sizeof(T) + iFloorPos.x];
+    T x11 = x[(iFloorPos.y + 1) * x_pitch / sizeof(T) + iFloorPos.x + 1];
 
-    if (j0 < 0) j0 = 0;
-    if (j0 >= height) j0 = height - 1;
+    T x0 = x00 * (1 - fracPos.x) + x01 * fracPos.x;
+    T x1 = x10 * (1 - fracPos.x) + x11 * fracPos.x;
+    T res = x0 * (1 - fracPos.y) + x1 * fracPos.y;
 
-    if (j1 < 0) j1 = 0;
-    if (j1 >= height) j1 = height - 1;
-
-    float x00 = x[j0][i0];
-    float x01 = x[j0][i1];
-    float x10 = x[j1][i0];
-    float x11 = x[j1][i1];
-
-    float dx = posX - i0;
-    float dy = posY - j0;
-
-    float x0 = x00 * (1 - dx) + x01 * dx;
-    float x1 = x10 * (1 - dx) + x11 * dx;
-
-    result[j][i] = x0 * (1 - dy) + x1 * dy;
+    result[j * result_pitch / sizeof(T) + i] = res;
 }
-
 
 /**
  * @brief The Jacobi Iteration Fragment Program Used to Solve Poisson Equations
- * 
+ *
  * @param result result
  * @param x x vector (Ax = b)
  * @param b b vector (Ax = b)
- * @param alpha 
+ * @param alpha
  * @param rBeta reciprocal beta
  * @param width array width
  * @param height array height
  */
-__global__ void jacobi_float3_kernel(float3 *result, size_t result_pitch,
-    float3 *x, size_t x_pitch,
-    float3 *b, size_t b_pitch,
-    float alpha, float rBeta, int width, int height) {
-    
+template <typename T>
+__global__ void jacobi_kernel(T *result, size_t result_pitch,
+                              T *x, size_t x_pitch,
+                              T *b, size_t b_pitch,
+                              float alpha, float rBeta, int width, int height) {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     int j = blockIdx.y * blockDim.y + threadIdx.y;
 
     if (i >= width || j >= height) return;
 
     if (i == 0 || j == 0 || i == width - 1 || j == height - 1) {
-        result[j * result_pitch / sizeof(float3) + i] = x[j * x_pitch / sizeof(float3) + i];
+        result[j * result_pitch / sizeof(T) + i] = x[j * x_pitch / sizeof(T) + i];
         return;
     }
 
     // left, right, bottom, and top x samples
-    float3 xL = x[j * x_pitch / sizeof(float3) + i - 1];
-    float3 xR = x[j * x_pitch / sizeof(float3) + i + 1];
-    float3 xB = x[(j - 1) * x_pitch / sizeof(float3) + i];
-    float3 xT = x[(j + 1) * x_pitch / sizeof(float3) + i];
+    T xL = x[j * x_pitch / sizeof(T) + i - 1];
+    T xR = x[j * x_pitch / sizeof(T) + i + 1];
+    T xB = x[(j - 1) * x_pitch / sizeof(T) + i];
+    T xT = x[(j + 1) * x_pitch / sizeof(T) + i];
 
     // b sample, from center
-    float3 bC = b[j * b_pitch / sizeof(float3) + i];
+    T bC = b[j * b_pitch / sizeof(T) + i];
 
     // evaluate Jacobi iteration
-    float3 xNew = (xL + xR + xB + xT + alpha * bC) * rBeta;
+    T xNew = (xL + xR + xB + xT + alpha * bC) * rBeta;
 
-    result[j * result_pitch / sizeof(float3) + i] = xNew;
+    result[j * result_pitch / sizeof(T) + i] = xNew;
 }
-__global__ void jacobi_float_kernel(float *result, size_t result_pitch,
-    float *x, size_t x_pitch,
-    float *b, size_t b_pitch,
-    float alpha, float rBeta, int width, int height) {
-    
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-    int j = blockIdx.y * blockDim.y + threadIdx.y;
-
-    if (i >= width || j >= height) return;
-
-    if (i == 0 || j == 0 || i == width - 1 || j == height - 1) {
-        result[j * result_pitch / sizeof(float) + i] = x[j * x_pitch / sizeof(float) + i];
-        return;
-    }
-
-    // left, right, bottom, and top x samples
-    float xL = x[j * x_pitch / sizeof(float) + i - 1];
-    float xR = x[j * x_pitch / sizeof(float) + i + 1];
-    float xB = x[(j - 1) * x_pitch / sizeof(float) + i];
-    float xT = x[(j + 1) * x_pitch / sizeof(float) + i];
-
-    // b sample, from center
-    float bC = b[j * b_pitch / sizeof(float) + i];
-
-    // evaluate Jacobi iteration
-    float xNew = (xL + xR + xB + xT + alpha * bC) * rBeta;
-
-    result[j * result_pitch / sizeof(float) + i] = xNew;
-}
-
 
 /**
  * @brief The Divergence Fragment Program
- * 
+ *
  * @param result divergence
  * @param halfrdx 0.5 / gridscale
  * @param wX vector field (x component)
  * @param wY vector field (y component)
  * @param width array width
  * @param height array height
- * @return __global__ 
+ * @return __global__
  */
 __global__ void divergence(float **result, float halfrdx, float **wX, float **wY, int width, int height) {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
@@ -198,7 +176,7 @@ __global__ void float3_to_uint8(uint8_t *result, float3 *input, size_t pitch, in
     if (i >= width || j >= height) return;
 
     float3 val = input[j * pitch / sizeof(float3) + i];
-    j = height - j - 1; // OpenGL flip-y
+    j = height - j - 1;  // OpenGL flip-y
 
     result[j * width * 4 + i * 4 + 0] = (uint8_t)(val.x * 255);
     result[j * width * 4 + i * 4 + 1] = (uint8_t)(val.y * 255);
@@ -231,14 +209,14 @@ __global__ void setup_dye_texture(int width, int height) {
     float3 c = make_float3(color, color, color);
 
     m_dye[0][y * m_dye_pitch[0] / sizeof(float3) + x] = c;
-    m_dye[1][y * m_dye_pitch[1] / sizeof(float3) + x] = c; 
+    m_dye[1][y * m_dye_pitch[1] / sizeof(float3) + x] = c;
 }
 
 void setup_fluids(int width, int height) {
-    checkCudaErrors(cudaMallocPitch(&h_velocity[0], &h_velocity_pitch[0], width * sizeof(float3), height));
-    checkCudaErrors(cudaMemset2D(h_velocity[0], h_velocity_pitch[0], 0, width * sizeof(float3), height));
-    checkCudaErrors(cudaMallocPitch(&h_velocity[1], &h_velocity_pitch[1], width * sizeof(float3), height));
-    checkCudaErrors(cudaMemset2D(h_velocity[1], h_velocity_pitch[1], 0, width * sizeof(float3), height));
+    checkCudaErrors(cudaMallocPitch(&h_velocity[0], &h_velocity_pitch[0], width * sizeof(float2), height));
+    checkCudaErrors(cudaMemset2D(h_velocity[0], h_velocity_pitch[0], 0, width * sizeof(float2), height));
+    checkCudaErrors(cudaMallocPitch(&h_velocity[1], &h_velocity_pitch[1], width * sizeof(float2), height));
+    checkCudaErrors(cudaMemset2D(h_velocity[1], h_velocity_pitch[1], 0, width * sizeof(float2), height));
 
     checkCudaErrors(cudaMallocPitch(&h_pressure[0], &h_pressure_pitch[0], width * sizeof(float), height));
     checkCudaErrors(cudaMemset2D(h_pressure[0], h_pressure_pitch[0], 0, width * sizeof(float), height));
@@ -261,22 +239,53 @@ void setup_fluids(int width, int height) {
     checkCudaErrors(cudaMemcpyToSymbol(m_divergence_pitch, &h_divergence_pitch, sizeof(h_divergence_pitch)));
     checkCudaErrors(cudaMemcpyToSymbol(m_dye, &h_dye, sizeof(h_dye)));
     checkCudaErrors(cudaMemcpyToSymbol(m_dye_pitch, &h_dye_pitch, sizeof(h_dye_pitch)));
-    
+
     dim3 gridDim((width + 31) / 32, (height + 31) / 32);
     dim3 blockDim(32, 32);
     setup_dye_texture<<<gridDim, blockDim>>>(width, height);
 }
 
-void advect_velocity(double timestep) {
-
+void advect_velocity(Controller *controller, double timestep) {
+    int width = controller->width;
+    int height = controller->height;
+    dim3 gridDim((width + 31) / 32, (height + 31) / 32);
+    dim3 blockDim(32, 32);
+    // 0 -> 1
+    boundary_advect_kernel<float2><<<gridDim, blockDim>>>(
+        h_velocity[1], h_velocity_pitch[1],
+        h_velocity[0], h_velocity_pitch[0],
+        -1.0f, width, height);
+    // 1 -> 0
+    float rdx = 1.0f / width;
+    advect_kernel<float2><<<gridDim, blockDim>>>(
+        h_velocity[0], h_velocity_pitch[0],
+        timestep, rdx,
+        h_velocity[1], h_velocity_pitch[1],
+        h_velocity[1], h_velocity_pitch[1],
+        width, height);
 }
 
-void advect_dye(double timestep) {
-    
+void advect_dye(Controller *controller, double timestep) {
+    int width = controller->width;
+    int height = controller->height;
+    dim3 gridDim((width + 31) / 32, (height + 31) / 32);
+    dim3 blockDim(32, 32);
+    // 0 -> 1
+    boundary_advect_kernel<float3><<<gridDim, blockDim>>>(
+        h_dye[1], h_dye_pitch[1],
+        h_dye[0], h_dye_pitch[0],
+        -1.0f, width, height);
+    // 1 -> 0
+    float rdx = 1.0f / width;
+    advect_kernel<float3><<<gridDim, blockDim>>>(
+        h_dye[0], h_dye_pitch[0],
+        timestep, rdx,
+        h_dye[1], h_dye_pitch[1],
+        h_velocity[1], h_velocity_pitch[1],
+        width, height);
 }
 
 void apply_force(Controller *controller) {
-
 }
 
 void diffuse_velocity(Controller *controller, double timestep) {
@@ -288,44 +297,37 @@ void diffuse_velocity(Controller *controller, double timestep) {
     float alpha = (2.0f / width) * (2.0f / height) / (m_viscosity * timestep);
     float rBeta = 1.0f / (4 + alpha);
     for (int i = 0; i < 15; i++) {
-        jacobi_float3_kernel<<<gridDim, blockDim>>>(
+        // 0 -> 1
+        jacobi_kernel<float2><<<gridDim, blockDim>>>(
+            h_velocity[1], h_velocity_pitch[1],
             h_velocity[0], h_velocity_pitch[0],
-            h_velocity[1], h_velocity_pitch[1],
-            h_velocity[1], h_velocity_pitch[1],
+            h_velocity[0], h_velocity_pitch[0],
             alpha, rBeta,
             width, height);
-        jacobi_float3_kernel<<<gridDim, blockDim>>>(
+        // 1 -> 0
+        jacobi_kernel<float2><<<gridDim, blockDim>>>(
             h_velocity[0], h_velocity_pitch[0],
             h_velocity[1], h_velocity_pitch[1],
             h_velocity[1], h_velocity_pitch[1],
             alpha, rBeta,
             width, height);
     }
-    jacobi_float3_kernel<<<gridDim, blockDim>>>(
-        h_velocity[0], h_velocity_pitch[0],
-        h_velocity[1], h_velocity_pitch[1],
-        h_velocity[1], h_velocity_pitch[1],
-        alpha, rBeta,
-        width, height);
 }
 
 void divergence() {
-
 }
 
 void computePressure() {
-
 }
 
 void gradientSubtraction() {
-
 }
 
 void update_fluids(Controller *controller, double timestep) {
     timestep = 0.1;
 
-    advect_velocity(timestep);
-    advect_dye(timestep);
+    advect_velocity(controller, timestep);
+    advect_dye(controller, timestep);
 
     if (controller->mouseButtons[GLFW_MOUSE_BUTTON_LEFT]) {
         apply_force(controller);
@@ -334,6 +336,6 @@ void update_fluids(Controller *controller, double timestep) {
     diffuse_velocity(controller, timestep);
 
     divergence();
-	computePressure();
-	gradientSubtraction();
+    computePressure();
+    gradientSubtraction();
 }
